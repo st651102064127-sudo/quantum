2fe81322fefe42de1a4dc845acd5ad0bab698a18
st651102064127-sudo <st651102064127@pcru.ac.th>
2026-03-04 02:57:55 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+// เปลี่ยนบรรทัดนี้: จาก BrowserRouter เป็น HashRouter
+import { HashRouter } from 'react-router-dom' 
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    {/* เปลี่ยนตรงนี้ด้วย */}
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
